48fa1f5469e726ffd411ef482d8d8ba5dc99922c
Martin Landa <landa.martin@gmail.com>
2020-04-24 10:39:12 +0200

    python script draft for v.random, see #3

---
+++ vector/v.random/testsuite/test_v_random.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import grass.script as gscript
+
+# v.random output=test01 npoints=100
+gscript.run_command('v.random', output='test01', npoints=100, overwrite=True)
+
+# 1. Rewrite into testsuite
+# 2. Check output vector map - number of points
