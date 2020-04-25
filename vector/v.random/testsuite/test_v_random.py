dec796385cb7d24a4832878268162cbcbfd2f261
JosefPudil <pepa.pudil@seznam.cz>
2020-04-25 15:48:51 +0200

    update test_v_random
    first try to make testsuit for v.random

--- vector/v.random/testsuite/test_v_random.py
+++ vector/v.random/testsuite/test_v_random.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
-
+#import grass.script as gscript
+from grass.gunittest.case import TestCase
+from grass.gunittest.main import test
 import grass.script as gscript
-
-# v.random output=test01 npoints=100
-gscript.run_command('v.random', output='test01', npoints=100, overwrite=True)
-
-# 1. Rewrite into testsuite
-# 2. Check output vector map - number of points
+
+class Test_v_random(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.use_temp_region()
+        cls.runModule('g.region',vector='boundary_state')
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.del_temp_region()
+
+
+    def test_points(self):
+        self.assertModule('v.random', output='test01', npoints=100, overwrite=True)
+
+
+if __name__ == '__main__':
+    test()
