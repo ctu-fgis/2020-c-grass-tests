fbeed6a40b2de88f94afe2dad30a0d718919b972
JosefPudil <pepa.pudil@seznam.cz>
2020-04-30 22:25:45 +0200

    Update test_v_random #3
    Change of input arguments, and adding test.  #3
    
    Co-Authored-By: dekanluc <dekanluc@users.noreply.github.com>

--- vector/v.random/testsuite/test_v_random.py
+++ vector/v.random/testsuite/test_v_random.py
@@ -3,13 +3,18 @@
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
 import grass.script as gscript
+
+output='test01'
+npoints=100
+state='boundary_state'
 
 class Test_v_random(TestCase):
+
 
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule('g.region',vector='boundary_state')
+        cls.runModule('g.region',vector=state)
 
 
     @classmethod
@@ -18,8 +23,9 @@ class Test_v_random(TestCase):
 
 
     def test_points(self):
-        self.assertModule('v.random', output='test01', npoints=100, overwrite=True)
-
+        self.assertModule('v.random', output=output, npoints=npoints, overwrite=True)
+        topology = dict(points=npoints)`
+        self.assertVectorFitsTopoInfo(vector=output, reference=topology)
 
 if __name__ == '__main__':
     test()
