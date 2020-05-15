729583ed0055a6b68783c2e2410054167adda90b
Martin Landa <landa.martin@gmail.com>
2020-05-15 13:18:26 +0200

    v.random: dos2unix

--- vector/v.random/testsuite/test_v_random.py
+++ vector/v.random/testsuite/test_v_random.py
@@ -3,28 +3,25 @@
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
 import grass.script as gscript
-
-output='test01'
-npoints=100
-state='boundary_state'
+
+output = 'test01'
+npoints = 100
+state = 'boundary_state'
 
 class Test_v_random(TestCase):
-
 
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule('g.region',vector=state)
-
+        cls.runModule('g.region', vector=state)
 
     @classmethod
     def tearDownClass(cls):
         cls.del_temp_region()
 
-
-    def test_points(self):
+    def test_num_points(self):
         self.assertModule('v.random', output=output, npoints=npoints, overwrite=True)
-        topology = dict(points=npoints)`
+        topology = dict(points=npoints)
         self.assertVectorFitsTopoInfo(vector=output, reference=topology)
 
 if __name__ == '__main__':
