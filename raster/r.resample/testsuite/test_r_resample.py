203c2d4138774a6ff2205681b3009d449538edd0
Martin Landa <landa.martin@gmail.com>
2020-06-02 15:38:38 +0200

    r.resample review

--- raster/r.resample/testsuite/test_r_resample.py
+++ raster/r.resample/testsuite/test_r_resample.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
-import grass.script as gscript
 
 output = 'test01'
 input = 'basins'
@@ -11,8 +9,8 @@ resolution = 20
 north = 228500
 south = 215000
 east = 645000
-west = 630000
-min=2
+west = 630000
+min=2
 max=30
 
 
@@ -21,33 +19,30 @@ class Test_r_resample(TestCase):
     @classmethod
 
     def setUpClass(cls):
-
         cls.use_temp_region()
-#change of resolution from 10 to 20 
-        cls.runModule('g.region', raster = input, res = resolution)
+
+        # change of resolution from 10 to 20 
+        cls.runModule('g.region', raster=input, res=resolution)
 
     @classmethod
-
     def tearDownClass(cls):
-
         cls.del_temp_region()
 
     def test_check_resample(self):
-#running funciton r.resample
-        self.assertModule('r.resample', input = input, output = output, overwrite = True)
-#testing resolution
-        self.assertModuleKeyValue('r.info', map = output, flags = 'gr',
-                          reference = dict(nsres = resolution , ewres = resolution),
-                          precision = 0.01, sep = '=')
-#testing coordinates
-        self.assertModuleKeyValue('r.info', map = output, flags = 'gr',
-                          reference = dict(north = north, west = west, east = east, south = south),
-                          precision = 0.01, sep = '=')
-#testing data values
-        self.assertModuleKeyValue('r.info', map = output, flags = 'gr',
-                          reference = dict(min=min , max=max),
-                          precision = 0.01, sep = '=')
+        # running r.resample
+        self.assertModule('r.resample', input=input, output=output, overwrite=True)
 
-if __name__ == '__main__':
-
+        # testing resolution
+        ref_res="nsres=20\newres=20"
+        self.assertRasterFitsInfo(raster=output, reference=ref_res)
+
+        # testing coordinates
+        ref_extent="north=228500\nsouth=215000\neast=645000\nwest=630000"
+        self.assertRasterFitsInfo(raster=output, reference=ref_extent)
+        
+        # testing data values
+        ref_values="min=2\nmax=30"
+        self.assertRasterFitsInfo(raster=output, reference=ref_values)
+
+if __name__ == '__main__':
     test()
