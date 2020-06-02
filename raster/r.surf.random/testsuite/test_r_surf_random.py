49dc39c8da9a907724f5f1e6be70f2e7a7b3f4cd
Martin Landa <landa.martin@gmail.com>
2020-06-02 15:24:38 +0200

    r.surf.random: define resolution

--- raster/r.surf.random/testsuite/test_r_surf_random.py
+++ raster/r.surf.random/testsuite/test_r_surf_random.py
@@ -2,31 +2,32 @@
 
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
-import grass.script as gscript
 
 output = 'test01'
 n=100
 s=-100
 w=-100
 e=100
+res=1
 min=10
 max=40
 
 class Test_v_surf_random(TestCase):
 
-#setting up computational region
+# setting up computational region
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule('g.region', n=n, s=s, e=e, w=w)
+        cls.runModule('g.region', n=n, s=s, e=e, w=w, res=res)
 
     @classmethod
     def tearDownClass(cls):
         cls.del_temp_region()
 
-#Testing if output parameters are between input parameters
+# testing if output parameters are between input parameters
     def test_min_max(self):
          self.assertModule('r.surf.random', output=output, min=min, max=max, overwrite=True)
          self.assertRasterMinMax(map=output, refmin=min, refmax=max)
+
 if __name__ == '__main__':
     test()
